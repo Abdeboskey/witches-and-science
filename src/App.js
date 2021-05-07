de0f747504b3715cbf7b8dbc915b2b6227db5eed
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2021-05-07 01:06:30 -0600

    Replace copy and icon with cool stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import seed from './assets/seed.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={seed} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	  <code>A creative coven in Denver, CO</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.witchesandscience.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Witches & Science 
         </a>
       </header>
     </div>
